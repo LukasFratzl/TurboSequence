41d8eaf7e5b55ff40d1027d0f7e34702e9f418a1
flow_____ <lukas.fratzl@gmail.com>
2024-05-12 17:46:21 +0200

    Added super simple demo footprint asset to learn from it

--- Source/TurboSequence_Lf/Private/TurboSequence_DemoSimple_FootprintAsset_Lf.cpp
+++ Source/TurboSequence_Lf/Private/TurboSequence_DemoSimple_FootprintAsset_Lf.cpp
@@ -1,8 +1,97 @@
-﻿$COPYRIGHT_LINE$
+﻿// Copyright Lukas Fratzl, 2022-2024. All Rights Reserved.
 
 
-$MY_HEADER_INCLUDE_DIRECTIVE$
+#include "TurboSequence_DemoSimple_FootprintAsset_Lf.h"
 
+#include "TurboSequence_Manager_Lf.h"
 
+void UTurboSequence_DemoSimple_FootprintAsset_Lf::OnSetMeshIsVisible_Concurrent(
+	ETurboSequence_IsVisibleOverride_Lf& IsVisibleOverride, const bool bDefaultVisibility, const int32 MeshID,
+	const TObjectPtr<UTurboSequence_ThreadContext_Lf>& ThreadContext)
+{
+	if (MeshDataCollection.Contains(MeshID) && IsValid(MeshDataCollection[MeshID]) && MeshDataCollection[MeshID]->
+		IsVisible())
+	{
+		IsVisibleOverride = ETurboSequence_IsVisibleOverride_Lf::IsNotVisible;
+	}
 
+	Super::OnSetMeshIsVisible_Concurrent(IsVisibleOverride, bDefaultVisibility, MeshID, ThreadContext);
+}
 
+void UTurboSequence_DemoSimple_FootprintAsset_Lf::OnSetMeshIsUpdatingLod_Concurrent(bool& bIsUpdatingLodOverride,
+	const int32 MeshID, const TObjectPtr<UTurboSequence_ThreadContext_Lf>& ThreadContext)
+{
+	if (MeshDataCollection.Contains(MeshID) && IsValid(MeshDataCollection[MeshID]) && MeshDataCollection[MeshID]->
+		IsVisible())
+	{
+		bIsUpdatingLodOverride = false;
+	}
+
+	Super::OnSetMeshIsUpdatingLod_Concurrent(bIsUpdatingLodOverride, MeshID, ThreadContext);
+}
+
+void UTurboSequence_DemoSimple_FootprintAsset_Lf::OnAddedMeshInstance_GameThread(const int32 MeshID,
+	const TObjectPtr<UTurboSequence_MeshAsset_Lf>& FromAsset)
+{
+	if (!MeshDataCollection.Contains(MeshID) && IsValid(UEMeshClass))
+	{
+		const TObjectPtr<AActor> Mesh =
+			UTurboSequence_Helper_BlueprintFunctions_Lf::TurboSequence_GetWorldFromStaticFunction()->SpawnActor<AActor>(
+				UEMeshClass, ATurboSequence_Manager_Lf::GetMeshWorldSpaceTransform_RawID_Concurrent(MeshID)
+			);
+
+		if (IsValid(Mesh))
+		{
+			const TObjectPtr<USkinnedMeshComponent> MeshComponent = Cast<USkinnedMeshComponent>(
+				Mesh->GetComponentByClass(USkinnedMeshComponent::StaticClass()));
+
+			if (IsValid(MeshComponent))
+			{
+				MeshDataCollection.Add(MeshID, MeshComponent);
+			}
+		}
+	}
+
+	Super::OnAddedMeshInstance_GameThread(MeshID, FromAsset);
+}
+
+void UTurboSequence_DemoSimple_FootprintAsset_Lf::OnRemovedMeshInstance_GameThread(const int32 MeshID,
+	const TObjectPtr<UTurboSequence_MeshAsset_Lf>& FromAsset)
+{
+	if (MeshDataCollection.Contains(MeshID))
+	{
+		MeshDataCollection[MeshID]->GetOwner()->Destroy();
+
+		MeshDataCollection.Remove(MeshID);
+	}
+
+	Super::OnRemovedMeshInstance_GameThread(MeshID, FromAsset);
+}
+
+void UTurboSequence_DemoSimple_FootprintAsset_Lf::OnManagerUpdated_GameThread(const float DeltaTime)
+{
+	for (const TTuple<uint32, TObjectPtr<USkinnedMeshComponent>> Mesh : MeshDataCollection)
+	{
+		if (IsValid(Mesh.Value))
+		{
+			const float CameraDistance =
+				ATurboSequence_Manager_Lf::GetMeshClosestCameraDistance_RawID_Concurrent(Mesh.Key);
+
+			const bool bShouldDrawUEMesh = CanShowUEMesh(CameraDistance);
+
+			Mesh.Value->SetVisibility(bShouldDrawUEMesh);
+
+			if (bShouldDrawUEMesh)
+			{
+				ATurboSequence_Manager_Lf::SetMeshWorldSpaceTransform_RawID_Concurrent(
+					Mesh.Key, Mesh.Value->GetComponentTransform());
+			}
+			else
+			{
+				Mesh.Value->SetWorldTransform(ATurboSequence_Manager_Lf::GetMeshWorldSpaceTransform_RawID_Concurrent(Mesh.Key));
+			}
+		}
+	}
+
+	Super::OnManagerUpdated_GameThread(DeltaTime);
+}
--- Source/TurboSequence_Lf/Public/TurboSequence_DemoSimple_FootprintAsset_Lf.h
+++ Source/TurboSequence_Lf/Public/TurboSequence_DemoSimple_FootprintAsset_Lf.h
@@ -1,1 +1,43 @@
-﻿
+﻿// Copyright Lukas Fratzl, 2022-2024. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TurboSequence_FootprintAsset_Lf.h"
+#include "Engine/DataAsset.h"
+#include "TurboSequence_DemoSimple_FootprintAsset_Lf.generated.h"
+
+/**
+ *
+ */
+UCLASS(BlueprintType)
+class TURBOSEQUENCE_LF_API UTurboSequence_DemoSimple_FootprintAsset_Lf : public UTurboSequence_FootprintAsset_Lf
+{
+public:
+	GENERATED_BODY()
+
+	TMap<int32, TObjectPtr<USkinnedMeshComponent>> MeshDataCollection;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> UEMeshClass;
+
+	UPROPERTY(EditAnywhere)
+	float UEMeshShowDistance = 1000.0f;
+
+
+	bool CanShowUEMesh(const float MeshDistanceToCamera) const
+	{
+		return MeshDistanceToCamera < UEMeshShowDistance;
+	}
+
+	virtual void OnSetMeshIsVisible_Concurrent(ETurboSequence_IsVisibleOverride_Lf& IsVisibleOverride,
+		const bool bDefaultVisibility, const int32 MeshID,
+		const TObjectPtr<UTurboSequence_ThreadContext_Lf>& ThreadContext) override;
+	virtual void OnSetMeshIsUpdatingLod_Concurrent(bool& bIsUpdatingLodOverride, const int32 MeshID,
+		const TObjectPtr<UTurboSequence_ThreadContext_Lf>& ThreadContext) override;
+	virtual void OnAddedMeshInstance_GameThread(const int32 MeshID,
+		const TObjectPtr<UTurboSequence_MeshAsset_Lf>& FromAsset) override;
+	virtual void OnRemovedMeshInstance_GameThread(const int32 MeshID,
+		const TObjectPtr<UTurboSequence_MeshAsset_Lf>& FromAsset) override;
+	virtual void OnManagerUpdated_GameThread(const float DeltaTime) override;
+};
